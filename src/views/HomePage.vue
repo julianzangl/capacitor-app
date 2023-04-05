8553eedf278fcadee6b31bccacbac29c15d5bd95
JuleZ_2k4 <8096@htl.rennweg.at>
2023-04-05 20:23:26 +0200

    added

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,32 +7,104 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <div id="container">
-        <strong>Welcome to Zangl tracking</strong>
-        <p>
-          This is a simple app to track your time.
-        </p>
-        {{ $store.state.count }}
-        <ion-button @click="$store.commit('increment')">Increment</ion-button>
-      </div>
+      <ion-modal ref="modal" trigger="open-modal" @willDismiss="onWillDismiss">
+        <ion-header>
+          <ion-toolbar>
+            <ion-buttons slot="start">
+              <ion-button @click="cancel()">Cancel</ion-button>
+            </ion-buttons>
+            <ion-title>Welcome</ion-title>
+            <ion-buttons slot="end">
+              <ion-button :strong="true" @click="confirm()">Confirm</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+          <ion-item>
+            <ion-label position="stacked">Enter your name</ion-label>
+            <ion-input
+              ref="input"
+              type="text"
+              placeholder="Your name"
+            ></ion-input>
+          </ion-item>
+        </ion-content>
+      </ion-modal>
+      <ion-fab slot="fixed" vertical="bottom" horizontal="end">
+        <ion-fab-button id="open-modal">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
+
+    <ion-footer>
+      <ion-toolbar>
+        <ion-input placeholder="Enter text..."></ion-input>
+        <ion-button slot="end">Cat fact</ion-button>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonButtons,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonModal,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonFooter,
+} from "@ionic/vue";
+import { add } from "ionicons/icons";
+import { OverlayEventDetail } from "@ionic/core/components";
+import { defineComponent, ref } from "vue";
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
-})
-
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonModal,
+    IonInput,
+    IonItem,
+    IonLabel,
+    IonFooter,
+    IonButtons,
+    IonButton,
+  },
+  setup() {
+    return { add };
+  },
+  methods: {
+    cancel() {
+      //check the type of the ref
+      console.log(this.$refs.modal);
+    },
+    confirm() {
+      // const name = this.$refs.input.$el.value;
+      // this.$refs.modal.$el.dismiss(name, "confirm");
+    },
+    onWillDismiss(ev: CustomEvent<OverlayEventDetail>) {
+      if (ev.detail.role === "confirm") {
+        // this.message = `Hello, ${ev.detail.data}!`;
+      }
+    },
+  },
+});
 </script>
 
 <style scoped>
@@ -41,7 +113,7 @@ export default defineComponent({
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  
+
   height: 100%;
   padding: 0 16px;
 }
@@ -54,13 +126,6 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
-  color: #8c8c8c;
-  
   margin: 0;
-}
-
-#container a {
-  text-decoration: none;
 }
 </style>
